97bc78d55dca8f277e6d37aff8cf5bec28a8939d
Xinhu Liu <xinhu.liu90@gmail.com>
2019-03-26 14:12:36 +0100

    Add initial content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-page';
+export class AppComponent implements OnInit {
+  count = '';
+  hearted = false;
+
+  private readonly endpoint = 'https://uclk91azmj.execute-api.eu-central-1.amazonaws.com/Prod/likes';
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    if (window.localStorage !== undefined) {
+      this.hearted = window.localStorage.getItem('hearted') === 'true';
+    }
+
+    this.httpClient.get(this.endpoint).toPromise().then(value => {
+      this.count = value.toString();
+    });
+  }
+
+  onHeartClick(): void {
+    if (this.hearted) { return; }
+
+    this.hearted = true;
+    this.count = (Number.parseInt(this.count, 10) + 1).toString();
+    this.httpClient.post(this.endpoint, undefined).toPromise().then(() => {
+      window.localStorage.setItem('hearted', 'true');
+    });
+  }
 }
